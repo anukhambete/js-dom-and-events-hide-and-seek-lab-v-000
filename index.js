4f89c6091e4d26a1aa9eb31630faddee578612fd
Anu Khambete <anukhambete@gmail.com>
2018-11-27 22:19:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ return document.querySelector(`${selector}`);
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
